646b55d920499d5e641f14d8bcad79ea33396752
alireza asrari <alirezaasrari1990@gmail.com>
2022-10-15 14:22:53 +0330

    add capability of changing route to home page in onclick event of last page

--- Front-End/TaskTime/src/app/components/current-work-day/dialog-leaving-work/dialog-leaving-work.component.ts
+++ Front-End/TaskTime/src/app/components/current-work-day/dialog-leaving-work/dialog-leaving-work.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TaskTimeService } from 'src/app/services/taskTime';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dialog-leaving-work',
@@ -9,9 +10,9 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./dialog-leaving-work.component.css']
 })
 export class DialogLeavingWorkComponent implements OnInit {
-  router: any;
     constructor(private fb: FormBuilder, private service: TaskTimeService,
-      @Inject(MAT_DIALOG_DATA) public data: any){}
+      @Inject(MAT_DIALOG_DATA) public data: any,
+      private router: Router){}
   public form: FormGroup;
   rating: number = 0;
   description: string = "";
@@ -23,6 +24,7 @@ export class DialogLeavingWorkComponent implements OnInit {
       description: description,
       employeeId: this.employeeId
     }).subscribe();
+    this.router.navigateByUrl('');
   }
   ngOnInit(): void {
     this.employeeId = this.data.id;
