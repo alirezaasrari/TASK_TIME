b2df3578d0168f0bdfe54db5a640e7d082045d68
alireza asrari <alirezaasrari1990@gmail.com>
2022-10-24 14:05:26 +0330

    comment extra code in current work and rest page html and .ts

--- Front-End/TaskTime/src/app/components/current-work-day/current-work-day.component.ts
+++ Front-End/TaskTime/src/app/components/current-work-day/current-work-day.component.ts
@@ -51,9 +51,9 @@ export class CurrentWorkDayComponent implements OnInit {
         employeeState: this.workState,
       })
       .subscribe();
-    this.service
-      .getEmployeeStateById(this.selectedId)
-      .subscribe((x) => (this.stateTime = x.date.split('T')[1].substr(0, 5)));
+    // this.service
+    //   .getEmployeeStateById(this.selectedId)
+    //   .subscribe((x) => (this.stateTime = x.date.split('T')[1].substr(0, 5)));
     this.disabledWork = true;
     this.disabledRest = false;
     this.showTime.workTimes.push(
